2f481cd07134e7a34f7a09c7b426ce9beefdacc4
sigmund <SigmundB>
2019-09-16 16:41:55 +0200

    test000

---
+++ proto/src/no/hiof/gruppefire/controller/StartController.java
@@ -0,0 +1,15 @@
+package no.hiof.gruppefire.controller;
+
+import javafx.fxml.FXML;
+
+public class StartController {
+
+    public StartController(){
+
+    }
+
+    @FXML
+    public void newArrangementClicked(){
+        System.out.println("test");
+    }
+}
