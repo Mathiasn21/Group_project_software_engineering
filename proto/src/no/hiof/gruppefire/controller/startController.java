1a38e64a9d446e08da0148392f25aabc78ab1f69
andersbss <and.strands@gmail.com>
2019-09-16 16:39:46 +0200

    test10000000
    .

--- proto/src/no/hiof/gruppefire/controller/startController.java
+++ proto/src/no/hiof/gruppefire/controller/startController.java
@@ -1,15 +1,6 @@
 package no.hiof.gruppefire.controller;
 
-import javafx.fxml.FXML;
-
 public class StartController {
 
-    public StartController(){
-
-    }
-
-    @FXML
-    public void newArrangementClicked(){
-        System.out.println("test");
-    }
+    
 }
