c5133a1c5d2df2564db942b07e2dd169de6354dd
andersbss <and.strands@gmail.com>
2019-09-16 14:18:51 +0200

    Test
    .

---
+++ proto/src/no/hiof/gruppefire/Main.java
@@ -0,0 +1,8 @@
+package no.hiof.gruppefire;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
---
+++ proto/src/no/hiof/gruppefire/model/Arrangement.java
@@ -0,0 +1,15 @@
+package no.hiof.gruppefire.model;
+
+public class Arrangement {
+
+    private String navn;
+    private String idrett;
+    private int antallPlasser;
+
+
+
+    public Arrangement(String navn, int antallPlasser){
+        this.navn = navn;
+        this.antallPlasser = antallPlasser;
+    }
+}
