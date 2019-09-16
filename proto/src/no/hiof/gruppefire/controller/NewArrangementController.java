5e1e5fb711b27ece19838249c40dc6dff91dc4b0
andersbss <and.strands@gmail.com>
2019-09-16 18:21:44 +0200

    Bugfix
    Fikset FXML-knapper

--- proto/src/no/hiof/gruppefire/controller/NewArrangementController.java
+++ proto/src/no/hiof/gruppefire/controller/NewArrangementController.java
@@ -1,8 +1,28 @@
 package no.hiof.gruppefire.controller;
 
-import no.hiof.gruppefire.model.Arrangement;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import java.io.IOException;
 
 public class NewArrangementController {
 
+    @FXML
+    public void saveClicked(ActionEvent actionEvent){
+        System.out.println("save");
+
+        
+
+    }
+
+    @FXML
+    public void cancelClicked(ActionEvent actionEvent){
+        System.out.println("cancel");
+    }
 
 }
