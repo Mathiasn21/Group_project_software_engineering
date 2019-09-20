eb25b80bcea9da98156f4cb2378a1c3856d40ab2
TheWoakus <Magnusmn@live.no>
2019-09-20 11:43:34 +0200

    Lagt til første testklasse for håndtering av data

---
+++ proto/src/no/hiof/gruppefire/tests/DataHandlingTest.java
@@ -0,0 +1,12 @@
+package no.hiof.gruppefire.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class DataHandlingTest {
+
+    @Test
+    public void testBlah() {
+        assertEquals(1,1);
+    }
+}
