c0134f477029090e7eb779c62bc2d8c62bb47725
Mathiasn21 <43379198+Mathiasn21@users.noreply.github.com>
2019-10-08 11:45:42 +0200

    Update InputValidation.java

--- src/main/java/no/hiof/set/gruppe/data/InputValidation.java
+++ src/main/java/no/hiof/set/gruppe/data/InputValidation.java
@@ -28,6 +28,7 @@ public class InputValidation {
 
 
         //Could be done with:[^\p{L}\p{Nd}]+
+        //
         if(Pattern.matches("[A-Za-z0-9]+", name) && name.length() < 50 && name.length() > 2)
             validations++;
         else
