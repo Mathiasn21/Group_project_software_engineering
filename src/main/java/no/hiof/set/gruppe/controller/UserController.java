ddb59f1dd55dfa540f6ba9277c18bf50b773b05a
Mathiasn21 <43379198+Mathiasn21@users.noreply.github.com>
2019-10-17 12:38:34 +0200

    Revert "Lagt til lett logikk for å melde av/på arrangementer"
    This reverts commit 7c3edbf8c6aad9989d02645234492f0cf77781ce.

--- src/main/java/no/hiof/set/gruppe/controller/UserController.java
+++ src/main/java/no/hiof/set/gruppe/controller/UserController.java
@@ -41,7 +41,7 @@ public class UserController extends Controller{
 
     private String title, name = "";
     private ObservableList<Arrangement> arrangementListObservableJoined, arrangementListObservableAvailable, arrangementListObservableFinished;
-    private FilteredList<Arrangement> filteredList, joinedFilteredList;
+    private FilteredList<Arrangement> filteredList;
     private Arrangement currentSelectedArrangement = null;
 
     // --------------------------------------------------//
@@ -49,7 +49,7 @@ public class UserController extends Controller{
     // --------------------------------------------------//
 
     @FXML
-    private ListView<Arrangement> finishedArrangementsListView, availableArrangementsListView = new ListView<>(), joinedArrangementsListView, myArrangementsListView = new ListView<>();
+    private ListView<Arrangement> finishedArrangementsListView, availableArrangementsListView = new ListView<>(), joinedArrangementsListView;
     @FXML
     private Text arrangementTitle, arrangementSport,arrangementAddress,arrangementDate,arrangementParticipants,arrangementGroup, arrangementDescription;
     @FXML
@@ -66,17 +66,11 @@ public class UserController extends Controller{
     private void onJoinClick(ActionEvent actionEvent){
         //Trenger logikk
         System.out.println("meld på");
-
-        arrangementListObservableJoined.add(availableArrangementsListView.getSelectionModel().getSelectedItem());
-        arrangementListObservableAvailable.remove(availableArrangementsListView.getSelectionModel().getSelectedItem());
     }
 
     private void onLeaveClick(ActionEvent actionEvent){
         //Trenger logikk
         System.out.println("meld av");
-
-        arrangementListObservableAvailable.add(myArrangementsListView.getSelectionModel().getSelectedItem());
-        arrangementListObservableJoined.remove(myArrangementsListView.getSelectionModel().getSelectedItem());
     }
 
     // --------------------------------------------------//
@@ -99,24 +93,13 @@ public class UserController extends Controller{
     private void populateAvailableArrangementListView(){
 
         arrangementListObservableAvailable = FXCollections.observableArrayList(DataHandler.getUserArrangements(User.ORGANIZER));
-        setUpFilteredListAvailable();
+        setUpFilteredList();
         availableArrangementsListView.refresh();
     }
 
-    private void setUpFilteredListAvailable(){
+    private void setUpFilteredList(){
         filteredList = arrangementListObservableAvailable.filtered(arrangement -> true);
         availableArrangementsListView.setItems(filteredList);
-    }
-
-    private void populateJoinedArrangementListView() {
-        arrangementListObservableJoined = FXCollections.observableArrayList();
-        setUpFilteredListJoined();
-        myArrangementsListView.refresh();
-    }
-
-    private void setUpFilteredListJoined() {
-        joinedFilteredList = arrangementListObservableJoined.filtered(arrangement -> true);
-        myArrangementsListView.setItems(joinedFilteredList);
     }
 
 
@@ -142,7 +125,6 @@ public class UserController extends Controller{
     public void initialize(URL location, ResourceBundle resources) {
         logOut.setOnAction(this::returnToMainWindow);
         populateAvailableArrangementListView();
-        populateJoinedArrangementListView();
         setupActionHandlers();
     }
 
