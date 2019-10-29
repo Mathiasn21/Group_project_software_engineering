21a051214506226d2f1ae957f9c8faca8afca016
Mathiasn21 <Mathiasn21@users.noreply.github.com>
2019-10-29 18:31:44 +0100

    Created a few more testers

--- src/main/java/no/hiof/set/gruppe/tests/TestAccessValidation.java
+++ src/main/java/no/hiof/set/gruppe/tests/TestAccessValidation.java
@@ -1,6 +1,21 @@
 package no.hiof.set.gruppe.tests;
 
+import no.hiof.set.gruppe.Exceptions.InvalidLoginInformation;
+import no.hiof.set.gruppe.data.Repository;
+import no.hiof.set.gruppe.model.user.ILoginInformation;
+import no.hiof.set.gruppe.model.user.LoginInformation;
+import no.hiof.set.gruppe.model.user.User;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.provider.Arguments;
+
+import java.util.stream.Stream;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 /**
  * This class is meant for testing validation and security around data access
@@ -8,14 +23,36 @@ import org.junit.jupiter.api.Test;
  */
 class TestAccessValidation {
 
-    @Test
-    void userloginSuccess(){
-
+    /**
+     * Generates illegal {@link LoginInformation}
+     * @return {@link Stream}
+     */
+    @NotNull
+    @Contract(pure = true)
+    private static Stream<Arguments> GenIllegalLoginInformation() {
+        return Stream.of(
+                arguments(new LoginInformation("test", "soVerySafePass")),
+                arguments(new LoginInformation("\00", "\00"))
+        );
     }
 
     @Test
-    void userloginFailed(){
+    void userloginSuccess(ILoginInformation loginInformation) throws InvalidLoginInformation {
+        User userDetails = Repository.getUserDetails(loginInformation);
+        assertTrue(userEqualsLoginInformation(userDetails, loginInformation));
+    }
+
+    @Test
+    void userloginFailed(ILoginInformation loginInformation){
+        assertThrows(InvalidLoginInformation.class, () -> Repository.getUserDetails(loginInformation));
+    }
+
+    @Test
+    void userRegister(){
 
     }
 
+    private boolean userEqualsLoginInformation(@NotNull User userDetails, @NotNull ILoginInformation loginInformation) {
+        return userDetails.getName().equals(loginInformation.getUserID()) && userDetails.getPass().equals(loginInformation.getPassHash());
+    }
 }
