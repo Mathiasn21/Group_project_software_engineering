7579dfbd0ca0edd38d39d83d4014c10e80a0341a
Mathiasn21 <Mathiasn21@users.noreply.github.com>
2019-10-29 18:37:47 +0100

    Update TestAccessValidation.java

--- src/main/java/no/hiof/set/gruppe/tests/TestAccessValidation.java
+++ src/main/java/no/hiof/set/gruppe/tests/TestAccessValidation.java
@@ -2,17 +2,19 @@ package no.hiof.set.gruppe.tests;
 
 import no.hiof.set.gruppe.Exceptions.InvalidLoginInformation;
 import no.hiof.set.gruppe.data.Repository;
+import no.hiof.set.gruppe.model.RegisterUser;
 import no.hiof.set.gruppe.model.user.ILoginInformation;
 import no.hiof.set.gruppe.model.user.LoginInformation;
 import no.hiof.set.gruppe.model.user.User;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
 
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
@@ -23,6 +25,25 @@ import static org.junit.jupiter.params.provider.Arguments.arguments;
  */
 class TestAccessValidation {
 
+    @Test
+    void userloginSuccess() throws InvalidLoginInformation {
+        ILoginInformation loginInformation = new LoginInformation("User", "Password2");
+        User userDetails = Repository.getUserDetails(loginInformation);
+        assertTrue(userEqualsLoginInformation(userDetails, loginInformation));
+    }
+
+    @ParameterizedTest
+    @MethodSource("GenIllegalLoginInformation")
+    void userloginFailed(ILoginInformation loginInformation){
+        assertThrows(InvalidLoginInformation.class, () -> Repository.getUserDetails(loginInformation));
+    }
+
+    @Test
+    void userRegister(){
+        Repository.addNewUser(new RegisterUser());
+    }
+
+
     /**
      * Generates illegal {@link LoginInformation}
      * @return {@link Stream}
@@ -32,24 +53,9 @@ class TestAccessValidation {
     private static Stream<Arguments> GenIllegalLoginInformation() {
         return Stream.of(
                 arguments(new LoginInformation("test", "soVerySafePass")),
-                arguments(new LoginInformation("\00", "\00"))
+                arguments(new LoginInformation("\00", "\00")),
+                arguments(new LoginInformation("User", "\00"))
         );
-    }
-
-    @Test
-    void userloginSuccess(ILoginInformation loginInformation) throws InvalidLoginInformation {
-        User userDetails = Repository.getUserDetails(loginInformation);
-        assertTrue(userEqualsLoginInformation(userDetails, loginInformation));
-    }
-
-    @Test
-    void userloginFailed(ILoginInformation loginInformation){
-        assertThrows(InvalidLoginInformation.class, () -> Repository.getUserDetails(loginInformation));
-    }
-
-    @Test
-    void userRegister(){
-
     }
 
     private boolean userEqualsLoginInformation(@NotNull User userDetails, @NotNull ILoginInformation loginInformation) {
