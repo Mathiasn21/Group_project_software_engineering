390b9409716772b9615f1f0dc733175d30a64e31
Mathiasn21 <43379198+Mathiasn21@users.noreply.github.com>
2019-10-30 11:54:53 +0100

    Added new test class for system functionality

---
+++ src/main/java/no/hiof/set/gruppe/tests/TestSystemFunctionality.java
@@ -0,0 +1,6 @@
+package no.hiof.set.gruppe.tests;
+
+public class TestSystemFunctionality {
+    //test returning the proper list in order from arrangement.
+    //Test that correct data still exists in the arrangement object.?????
+}
