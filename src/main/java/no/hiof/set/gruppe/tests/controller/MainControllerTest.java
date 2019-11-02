064ccb8031488693676a9dfff914a082445dce6c
Mathiasn21 <Mathiasn21@users.noreply.github.com>
2019-11-02 14:17:05 +0100

    Refactored MainControllerTest.java
    Reduced the overall amount of code and is now more expandable.

--- src/main/java/no/hiof/set/gruppe/tests/controller/MainControllerTest.java
+++ src/main/java/no/hiof/set/gruppe/tests/controller/MainControllerTest.java
@@ -3,6 +3,7 @@ package no.hiof.set.gruppe.tests.controller;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import no.hiof.set.gruppe.model.user.ProtoUser;
 import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -21,28 +22,25 @@ class MainControllerTest extends MainJavaFXTest{
 
     @Test
     void enter_correct_login_information_from_btns(@NotNull FxRobot robot){
-        robot.clickOn("#adminLogin");
-        Assertions.assertThat(robot.lookup("#uName").queryAs(TextField.class)).hasText("Admin");
-        Assertions.assertThat(robot.lookup("#pass").queryAs(TextField.class)).hasText("Password1");
+        String[] userBtns = {"adminLogin", "arrangLogin", "userLogin"};
+        String[] stageTitles = {"ADMIN", "Arrangør", "Bruker"};
+        ProtoUser[] users = {ProtoUser.ADMIN, ProtoUser.ORGANIZER, ProtoUser.USER};
 
-        robot.clickOn("#logInn");
-        Assertions.assertThat(robot.lookup("#mainTitle").queryAs(Text.class)).hasText("ADMIN");
-        robot.clickOn("#logOut");
+        for(int i = 0; i < userBtns.length; i++){
+            firstAssertUserInformation(userBtns[i], users[i], robot);
+            thenAssertLoginHasCorrectWindow(robot, stageTitles[i]);
+        }
+    }
 
+    private void thenAssertLoginHasCorrectWindow(@NotNull FxRobot usingRobot, String stageTitle){
+        usingRobot.clickOn("#logInn");
+        Assertions.assertThat(usingRobot.lookup("#mainTitle").queryAs(Text.class)).hasText(stageTitle);
+        usingRobot.clickOn("#logOut");
+    }
 
-        robot.clickOn("#arrangLogin");
-        Assertions.assertThat(robot.lookup("#uName").queryAs(TextField.class)).hasText("Organizer");
-        Assertions.assertThat(robot.lookup("#pass").queryAs(TextField.class)).hasText("Password3");
-        robot.clickOn("#logInn");
-        Assertions.assertThat(robot.lookup("#mainTitle").queryAs(Text.class)).hasText("Arrangør");
-        robot.clickOn("#logOut");
-
-
-        robot.clickOn("#userLogin");
-        Assertions.assertThat(robot.lookup("#uName").queryAs(TextField.class)).hasText("ProtoUser");
-        Assertions.assertThat(robot.lookup("#pass").queryAs(TextField.class)).hasText("Password2");
-        robot.clickOn("#logInn");
-        Assertions.assertThat(robot.lookup("#mainTitle").queryAs(Text.class)).hasText("Bruker");
-        robot.clickOn("#logOut");
+    private void firstAssertUserInformation(@NotNull String userBtn, @NotNull ProtoUser user, @NotNull FxRobot usingRobot){
+        usingRobot.clickOn("#" + userBtn);
+        Assertions.assertThat(usingRobot.lookup("#uName").queryAs(TextField.class)).hasText(user.getName());
+        Assertions.assertThat(usingRobot.lookup("#pass").queryAs(TextField.class)).hasText(user.getPass());
     }
 }
