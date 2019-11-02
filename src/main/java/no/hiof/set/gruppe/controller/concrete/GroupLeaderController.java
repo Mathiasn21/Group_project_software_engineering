e31730ad997de8869395f590ac096e77b490836c
andersbss <and.strands@gmail.com>
2019-11-02 10:50:25 +0100

    Started adding functionality for groups

---
+++ src/main/java/no/hiof/set/gruppe/controller/concrete/GroupLeaderController.java
@@ -0,0 +1,34 @@
+package no.hiof.set.gruppe.controller.concrete;
+
+import no.hiof.set.gruppe.Exceptions.DataFormatException;
+import no.hiof.set.gruppe.controller.abstractions.Controller;
+import no.hiof.set.gruppe.model.ViewInformation;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class GroupLeaderController extends Controller {
+
+
+
+
+    @Override
+    public Object getDataObject() {
+        return null;
+    }
+
+    @Override
+    public void setDataFields(Object controller) throws DataFormatException {
+
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    @Override
+    public ViewInformation getViewInformation() {
+        return null;
+    }
+}
---
+++ src/main/java/no/hiof/set/gruppe/model/Group.java
@@ -0,0 +1,6 @@
+package no.hiof.set.gruppe.model;
+
+public class Group {
+
+
+}
