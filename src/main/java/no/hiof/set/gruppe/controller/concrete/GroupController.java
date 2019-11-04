912a7a4eb988d781cf17f81574aac3077decdbdd
andersbss <and.strands@gmail.com>
2019-11-04 18:20:03 +0100

    GroupController ready for logic

--- src/main/java/no/hiof/set/gruppe/controller/concrete/GroupController.java
+++ src/main/java/no/hiof/set/gruppe/controller/concrete/GroupController.java
@@ -14,8 +14,17 @@ package no.hiof.set.gruppe.controller.concrete;
 // --------------------------------------------------//
 //                1.Import Statements                //
 // --------------------------------------------------//
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.MenuItem;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
 import no.hiof.set.gruppe.Exceptions.DataFormatException;
 import no.hiof.set.gruppe.controller.abstractions.Controller;
+import no.hiof.set.gruppe.model.Group;
 import no.hiof.set.gruppe.model.ViewInformation;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -25,14 +34,54 @@ public class GroupController extends Controller {
     // --------------------------------------------------//
     //                2.Local Fields                     //
     // --------------------------------------------------//
-
+    private String title = "";
+    private String name = "";
+    private ObservableList<Group>groupsList;
+    private Group currentSeletedGroup = null;
     // --------------------------------------------------//
     //                3.FXML Fields                      //
     // --------------------------------------------------//
 
+    @FXML
+    private Button newGroupBtn, deleteBtn, editBtn;
+    @FXML
+    private MenuItem myIndividualArrangementsBtn, myGroupsBtn, logoutBtn;
+    @FXML
+    private ListView<Group> groupsListview;
+    @FXML
+    private Text members, groupName;
+
     // --------------------------------------------------//
     //                4.On Action Methods                //
     // --------------------------------------------------//
+
+    private void onClicknewGroupBtn(ActionEvent event){
+        System.out.println("ny");
+    }
+
+    private void onClickDeletBtn(ActionEvent event){
+        System.out.println("slett");
+    }
+
+    private void onClickEditBtn(ActionEvent event){
+        System.out.println("rediger");
+    }
+
+    private void onClickMyIndividualArrangementsBtn(ActionEvent event){
+        System.out.println("mine individuelle arrangementer");
+    }
+
+    private void onClickMyGroupsBtn(ActionEvent event){
+        System.out.println("mine grupper");
+    }
+
+    private void onClickLogoutBtn(ActionEvent event){
+        System.out.println("logg ut");
+    }
+
+    private void onClickGroupsListView(MouseEvent event){
+        System.out.println("liste");
+    }
 
     // --------------------------------------------------//
     //                5.Private Functional Methods       //
@@ -46,13 +95,25 @@ public class GroupController extends Controller {
     //                7.Private Setup Methods            //
     // --------------------------------------------------//
 
+    private void setupActionHandlers(){
+        newGroupBtn.setOnAction(this::onClicknewGroupBtn);
+        deleteBtn.setOnAction(this::onClickDeletBtn);
+        editBtn.setOnAction(this::onClickEditBtn);
+        groupsListview.setOnMouseClicked(this::onClickGroupsListView);
+
+        //Menu items
+        myIndividualArrangementsBtn.setOnAction(this::onClickMyIndividualArrangementsBtn);
+        myGroupsBtn.setOnAction(this::onClickMyGroupsBtn);
+        logoutBtn.setOnAction(this::onClickLogoutBtn);
+    }
+
     // --------------------------------------------------//
     //                8.Overridden Methods               //
     // --------------------------------------------------//
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+        setupActionHandlers();
     }
 
     @Override
